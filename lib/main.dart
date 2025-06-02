0a2efe981eb63b58b2b008eedf035be21e242476
Falcon0516 <eshanh051116@gmail.com>
2025-06-02 10:58:07 +0530

    Update main.dart by Akshay,Eshan,Divya D

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,93 @@
 import 'package:flutter/material.dart';
+import 'screens/HomePage.dart';
+import 'screens/SearchPage.dart';
+import 'screens/LibraryPage.dart';
+import 'screens/PremiumPage.dart';
+import 'screens/CreatePage.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const SpotifyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class SpotifyApp extends StatelessWidget {
+  const SpotifyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Spotify Replica',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.black,
+        scaffoldBackgroundColor: Colors.black,
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          backgroundColor: Colors.black,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.grey,
         ),
+      ),
+      home: const MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+  final PageController _pageController = PageController();
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+      _pageController.jumpToPage(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: const [
+          HomePage(),
+          SearchPage(),
+          LibraryPage(),
+          PremiumPage(),
+          CreatePage(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        backgroundColor: Colors.black,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.library_music),
+            label: 'Your Library',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.workspace_premium),
+            label: 'Premium',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.add_box), label: 'Create'),
+        ],
       ),
     );
   }
