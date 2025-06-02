d5a310a1b62f11b3430a739369d6368331289858
Hamsa2909 <hamsaprasad2909@gmail.com>
2025-06-02 11:08:31 +0530

    CreatePage.dart designed by hamsa

---
+++ lib/screens/CreatePage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreatePage extends StatelessWidget {
+  const CreatePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Text('Create Page', style: TextStyle(color: Colors.white)),
+      ),
+    );
+  }
+}
